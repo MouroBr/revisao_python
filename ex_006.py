6f9c296144619669afcdb2f54baf6975666cc9c7
Bruno Brizolla Do Nascimento <104790790+MouroBr@users.noreply.github.com>
2022-11-25 18:52:05 -0300

    Verifica o dobro, triplo e raiz quadrada de um numero

---
+++ ex_006.py
@@ -0,0 +1,8 @@
+# Verifica o dobro, triplo e raiz quadrada de um numero
+x = int(input('Digite um numero:'))
+
+dobro = int(x * 2)
+triplo = int(x * 3)
+raiz = int(x ** 0.5)
+
+print(f'O dobro de {x} = {dobro}\nO triplo de {x} = {triplo}\nA raiz quadrada de {x} = {raiz}')
