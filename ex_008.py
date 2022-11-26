cc076814fb84abe5c0ac81078941954d9ea9e91f
Bruno Brizolla Do Nascimento <104790790+MouroBr@users.noreply.github.com>
2022-11-25 21:13:39 -0300

    # Conversor de medidas

---
+++ ex_008.py
@@ -0,0 +1,4 @@
+# Conversor de medidas
+x = float(input('Digite a medida em metros:'))
+
+print(f'{x}m = {x / 1000}Km\n{x}m = {x / 100}hm\n{x}m = {x / 10}dam\n{x}m = {x}m\n{x}m = {x * 10}dm\n{x}m = {x * 100}cm\n{x}m = {x *1000}mm')
