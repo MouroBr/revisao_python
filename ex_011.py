f035e3273f98d1b775ae6d049525d9cf5a1eb550
Bruno Brizolla Do Nascimento <104790790+MouroBr@users.noreply.github.com>
2022-11-26 11:54:02 -0300

    Calcula a quantidade de tinta para pintar uma parede

--- ex_011.py
+++ ex_011.py
@@ -1,9 +1,9 @@
 # Calcula a quantidade de tinta para pintar uma parede
 largura = float(input('Digite a largura da parede:'))
 altura = float(input('Digite a altura da parede: '))
-tinta = float(input('Digite o rendimento da tinta: m^2/l'))
+
 
 area_parede = round(float(largura * altura), 2)
-rendimento = round(float(area_parede / tinta))
+rendimento = round(float(area_parede / 2))
 
 print(f'A área {area_parede} a quantidade de tinta necessária para pinta a parede é {rendimento}')
