954031596eaba3a7f492456f947cec4b64bd9a28
Bruno Brizolla Do Nascimento <104790790+MouroBr@users.noreply.github.com>
2022-12-20 01:36:12 -0300

    Descobre se é par ou impar(corrigido)

--- ex_029.py
+++ ex_029.py
@@ -1,5 +1,6 @@
 # Descobre se é par ou impar
-num = int(input('Digite um numero qualquer: '))
+
+num = int(input('Digite um número qualquer: '))
 valor = num % 2
 if valor == 0:
     print(f'O número {num} é par.')
