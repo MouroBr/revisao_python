74890c9e079971b1ea341c862d9f21444c20a635
Bruno Brizolla Do Nascimento <104790790+MouroBr@users.noreply.github.com>
2022-12-20 01:33:32 -0300

    Calculadoura de multas

---
+++ ex_029.py
@@ -0,0 +1,9 @@
+# Descobre se é par ou impar
+num = int(input('Digite um numero qualquer: '))
+valor = num % 2
+if valor == 0:
+    print(f'O número {num} é par.')
+else:
+    print(f'O número {num} é impar.')
+
+
