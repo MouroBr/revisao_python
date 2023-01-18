4987de10b088fe0f034b0b2d1463a5347908f19a
Bruno Brizolla Do Nascimento <104790790+MouroBr@users.noreply.github.com>
2023-01-18 17:22:41 -0300

    Jogo de Advinhação

---
+++ ex_037.py
@@ -0,0 +1,17 @@
+num = int(input(f'Diite um numero inteiro: '))
+print('''Escolha a base para conversão:
+[ 1 ] BINÁRIO
+[ 2 ] OCTAL
+[ 3 ] HEXADECIMAL''')
+opcao = int(input('Digite sua opção: '))
+if opcao == 1:
+    print(f'Em BINÁRIO {num} = {bin(num)}')
+elif opcao == 2:
+    print(f'Em OCTAL {num} = {oct(num)}')
+elif opcao == 3:
+    print(f'Em HEXADECIMAL {num} = {hex(num)}')
+else:
+    print('\033[0;31mOpção Invalida\033[m')
+
+
+
