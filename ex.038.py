99d25ae1f7831f7a6d5568b89660302064961fda
Bruno Brizolla Do Nascimento <104790790+MouroBr@users.noreply.github.com>
2023-01-18 18:02:10 -0300

    VErifica se um numero é maior ou menor

---
+++ ex.038.py
@@ -0,0 +1,10 @@
+def maiorOuMenor(a, b):
+    if a > b:
+        print('O primeiro valor é maior')
+    elif a < b:
+        print('O segundo valor é maior')
+    else:
+        print('Não existe valor maior, os dois são iguais')
+
+
+maiorOuMenor(3, 4)
